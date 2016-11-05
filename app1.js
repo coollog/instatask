b013a06d1be6c5df1e3cf47f33519394aee54813
Arjun <arjun.av@gmail.com>
2016-11-05 16:03:39 +0000

    Createtask v2

--- app1.js
+++ app1.js
@@ -7,12 +7,6 @@ var test = require('assert');
 
 // Connection url
 var mongoURI = 'mongodb://instatask:firstletters@ds059316.mlab.com:59316/instatask';
-var userModel = null;
-// Connect using MongoClient
-MongoClient.connect(mongoURI, function(err, db) {
-  userModel = new UserModel(db);
-});
-
 
 app.use(bodyParser.urlencoded({
 	extended: false
@@ -25,18 +19,33 @@ app.get('/',function(req,res){
     res.sendFile(path.join(__dirname,'createTask.html'));
 });
 
-app.post('/createTask', function(req,res){
-  //  console.log(JSON.stringify(req.body));
-    var urlParamString = "jh="+ encodeURIComponent(req.body.job_heading) + "&jd=" + 
-                            encodeURIComponent(req.body.job_description) + "&pay=" + encodeURIComponent(req.body.pay);
-    res.sendFile(path.join(__dirname,'testgeo.html'));
+app.get('/taskSuccess',function(req,res){
+    res.sendFile(path.join(__dirname,'taskCreateSuccess.html'));
 });
 
-/*
-app.get('/getLoc', function(req,res){
-   if(req.query.jh)
-   
-   console.log(req.query.jh);
-   console.log(req.query.jd);
-   console.log(req.query.pay);
-});*/
+app.post('/postTask', function(req,res){
+    console.log(JSON.stringify(req.body));
+    MongoClient.connect(mongoURI, function(err, db) {
+    if(err)
+    {
+        console.log("Error connecting to DB",err);
+    }
+    else
+    {
+        console.log("Connected to DB");
+        var task = {
+            title: req.body.jobHeading,
+            employer: "N/A",
+            employee: null,
+            description: req.body.jobDesc,
+            latitude: req.body.lat,
+            longitude: req.body.lng,
+            payment: req.body.pay,
+            status: "open"
+        }
+        db.collection('tasks').insertOne(task);   
+    }
+    db.close();
+});
+    console.log("Success");
+});
