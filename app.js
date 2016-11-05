b8d6212f88701ed6fd5271db6a19da6ede037102
Qingyang Chen <qingyang.chen@gmail.com>
2016-11-05 12:17:46 -0400

    register/login

--- app.js
+++ app.js
@@ -16,15 +16,35 @@ MongoClient.connect(mongoURI, function(err, db) {
 var UserModel = function(db) {
   this.collection = db.collection('users');
 };
-UserModel.prototype.insert = function(name, phone, email) {
-  this.collection.insert({
+UserModel.prototype.insert = function(name, pass, phone, email, callback) {
+  this.collection.insertOne({
     name: name,
+    pass: pass,
     phone: phone,
     email: email
   }, function(err, result) {
     test.equal(null, err);
+    callback(result.insertedId);
   });
 };
+UserModel.prototype.find = function(email, pass, callback) {
+  this.collection.findOne({
+    email: email,
+    pass: pass
+  }, function(err, result) {
+    test.equal(null, err);
+    callback(result._id);
+  });
+};
+
+function checkLogin(res) {
+  if (req.session.user) return true;
+
+  res.send({
+    redirect: 'login'
+  });
+  return false;
+}
 
 
 var app = express();
@@ -32,12 +52,21 @@ app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
+// For sessions.
+var session = require('express-session');
+app.use(session({
+  name: 'server-session-cookie-id',
+  secret: 'my express secret',
+  resave: true,
+  saveUninitialized: true
+}));
 
 app.get('/', function(req, res) {
-  res.send(fs.readFileSync('register.html', 'utf8'));
+
 });
 
 app.get('/working', function(req, res) {
+  if (!checkLogin(req, res)) return;
   res.send(fs.readFileSync('working.html', 'utf8'));
 });
 
@@ -45,12 +74,48 @@ app.get('/finishworking', function(req, res) {
   res.send('done');
 });
 
-app.get('/workinglocations')
+app.get('/workinglocations');
+app.get('/workerlocation');
+
+app.get('/register', function(req, res) {
+  res.send(fs.readFileSync('register.html', 'utf8'));
+});
+app.get('/login', function(req, res) {
+  res.send(fs.readFileSync('login.html', 'utf8'));
+});
 
 app.post('/register', function(req, res) {
-  if (!userModel) res.send('crap');
-  userModel.insert(req.body.name, req.body.phone, req.body.email);
-  res.send('success');
+  if (!userModel) {
+    return res.send({ error: 'Unexpected error.' });
+  }
+
+  var pass = req.body.pass;
+  var pass2 = req.body.pass2;
+
+  if (pass != pass2) {
+    return res.send({ error: 'Password mismatch.' });
+  }
+
+  userModel.insert(
+      req.body.name, req.body.pass, req.body.phone, req.body.email,
+      function(_id) {
+        req.session.user = _id;
+        res.send({});
+      });
+});
+
+app.post('/login', function(req, res) {
+  if (!userModel) {
+    return res.send({ error: 'Unexpected error.' });
+  }
+
+  var email = req.body.email;
+  var pass = req.body.pass;
+
+  userModel.find(email, pass, function(_id) {
+    req.session.user = _id;
+    res.send({});
+  });
 });
 
 app.listen(3000);
